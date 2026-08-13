63f42fd741099dfb93c702ad7f7e19a9a7c6c263
akeh-beep <akeh1760@gmail.com>
2026-08-13 14:39:49 -0700

    Complete banking system testing

--- models/database.js
+++ models/database.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/mockbank")
-.then(() => {
-    console.log("✅ MongoDB Connected");
-})
-.catch((err) => {
-    console.log(err);
-});
-  
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("✅ MongoDB Connected");
+    })
+    .catch((err) => {
+        console.error("❌ MongoDB Connection Error:", err);
+    });
+ 
